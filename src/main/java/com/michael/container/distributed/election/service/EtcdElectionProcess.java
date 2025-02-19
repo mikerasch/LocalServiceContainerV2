4470f222477d0545b0ea9ce6121dbdfc6d3d5067
Mike <54646452+mikerasch@users.noreply.github.com>
2025-02-18 21:34:48 -0500

    Feature/michael/start of leader follower (#2)
    * Leader Follower
    
    * Better log messages
    
    * Standalone docker compose and better logging
    
    * Postman Docker has ETCD now

---
+++ src/main/java/com/michael/container/distributed/election/service/EtcdElectionProcess.java
@@ -0,0 +1,128 @@
+package com.michael.container.distributed.election.service;
+
+import com.michael.container.distributed.election.config.EtcdConfiguration;
+import com.michael.container.distributed.election.enums.Role;
+import com.michael.container.distributed.election.model.LeaderKeyDeletionEvent;
+import com.michael.container.distributed.election.observer.LeaseRenewalStreamObserver;
+import com.michael.container.distributed.election.state.ElectionState;
+import io.etcd.jetcd.ByteSequence;
+import io.etcd.jetcd.Client;
+import io.etcd.jetcd.kv.TxnResponse;
+import io.etcd.jetcd.lease.LeaseGrantResponse;
+import io.etcd.jetcd.op.Cmp;
+import io.etcd.jetcd.op.CmpTarget;
+import io.etcd.jetcd.op.Op;
+import io.etcd.jetcd.options.PutOption;
+import io.etcd.jetcd.options.WatchOption;
+import io.etcd.jetcd.watch.WatchEvent;
+import io.etcd.jetcd.watch.WatchResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EtcdElectionProcess implements ElectionProcess {
+  private static final Logger logger = LoggerFactory.getLogger(EtcdElectionProcess.class);
+  private final Client etcdClient;
+  private final EtcdConfiguration etcdConfiguration;
+  private final ElectionState electionState;
+  private final ApplicationEventPublisher eventPublisher;
+
+  public EtcdElectionProcess(
+      Client etcdClient,
+      EtcdConfiguration etcdConfiguration,
+      ApplicationEventPublisher eventPublisher) {
+    this.etcdClient = etcdClient;
+    this.etcdConfiguration = etcdConfiguration;
+    this.electionState = new ElectionState();
+    this.eventPublisher = eventPublisher;
+  }
+
+  @Override
+  public void releaseLeadership() {
+    try {
+      etcdClient.getLeaseClient().revoke(electionState.getLeaseId()).get();
+      electionState.reset();
+    } catch (ExecutionException | InterruptedException | IOException e) {
+      logger.warn("Release leadership exception.", e);
+    }
+  }
+
+  @Override
+  public void startLeaderElection() {
+    determineRole();
+  }
+
+  private void determineRole() {
+    boolean acquiredLeadership = false;
+    LeaseGrantResponse leaseResponse = etcdClient.getLeaseClient().grant(5L).join();
+    electionState.setLeaseId(leaseResponse.getID());
+    ByteSequence key =
+        ByteSequence.from(etcdConfiguration.getEtcdLeaderKey(), StandardCharsets.UTF_8);
+    ByteSequence value =
+        ByteSequence.from(
+            etcdConfiguration.getServiceUniqueIdentifier().toString(), StandardCharsets.UTF_8);
+
+    Cmp keyExists = new Cmp(key, Cmp.Op.EQUAL, CmpTarget.version(0));
+
+    PutOption putOption = PutOption.builder().withLeaseId(electionState.getLeaseId()).build();
+
+    try {
+      TxnResponse txnResponse =
+          etcdClient
+              .getKVClient()
+              .txn()
+              .If(keyExists)
+              .Then(Op.put(key, value, putOption))
+              .commit()
+              .get();
+
+      if (txnResponse.isSucceeded()) {
+        acquiredLeadership = true;
+        logger.info("Server elected as leader...");
+        startLeaseRenewal();
+      } else {
+        logger.info("Server elected as follower...");
+      }
+    } catch (ExecutionException | InterruptedException e) {
+      logger.warn("Could not acquire leadership", e);
+    }
+    electionState.setRole(acquiredLeadership ? Role.LEADER : Role.FOLLOWER);
+    listenForKeyDeletion();
+  }
+
+  private void listenForKeyDeletion() {
+    ByteSequence key =
+        ByteSequence.from(etcdConfiguration.getEtcdLeaderKey(), StandardCharsets.UTF_8);
+    electionState.setWatchCloseable(
+        etcdClient
+            .getWatchClient()
+            .watch(key, WatchOption.builder().withNoPut(true).build(), this::onDeletionOfKey));
+  }
+
+  private void onDeletionOfKey(WatchResponse watchResponse) {
+    if (watchResponse.getEvents().stream()
+        .anyMatch(event -> event.getEventType() == WatchEvent.EventType.DELETE)) {
+      logger.info("Leader deletion occurred.");
+      try {
+        electionState.reset();
+      } catch (IOException e) {
+        logger.error("Error resetting the election state after leader deletion.", e);
+      } finally {
+        CompletableFuture.runAsync(() -> eventPublisher.publishEvent(new LeaderKeyDeletionEvent()));
+      }
+    }
+  }
+
+  private void startLeaseRenewal() {
+    electionState.setLeaseCloseableClient(
+        etcdClient
+            .getLeaseClient()
+            .keepAlive(electionState.getLeaseId(), new LeaseRenewalStreamObserver()));
+  }
+}
---
+++ src/main/java/com/michael/container/distributed/election/state/ElectionState.java
@@ -0,0 +1,56 @@
+package com.michael.container.distributed.election.state;
+
+import com.michael.container.distributed.election.enums.Role;
+import io.etcd.jetcd.support.CloseableClient;
+import java.io.Closeable;
+import java.io.IOException;
+
+public class ElectionState {
+  private Long leaseId;
+  private CloseableClient leaseCloseableClient;
+  private Closeable watchCloseable;
+  private Role role;
+
+  public Long getLeaseId() {
+    return leaseId;
+  }
+
+  public void setLeaseId(long leaseId) {
+    this.leaseId = leaseId;
+  }
+
+  public CloseableClient getLeaseCloseableClient() {
+    return leaseCloseableClient;
+  }
+
+  public void setLeaseCloseableClient(CloseableClient leaseCloseableClient) {
+    this.leaseCloseableClient = leaseCloseableClient;
+  }
+
+  public Role getRole() {
+    return role;
+  }
+
+  public void setRole(Role role) {
+    this.role = role;
+  }
+
+  public Closeable getWatchCloseable() {
+    return watchCloseable;
+  }
+
+  public void setWatchCloseable(Closeable watchCloseable) {
+    this.watchCloseable = watchCloseable;
+  }
+
+  public void reset() throws IOException {
+    leaseId = null;
+    role = null;
+    if (leaseCloseableClient != null) {
+      leaseCloseableClient.close();
+    }
+    if (watchCloseable != null) {
+      watchCloseable.close();
+    }
+  }
+}
