a680532643305814b4e8978a3001a1732dc92baa
Michael Rasch <raschmichael10@gmail.com>
2025-02-22 21:43:28 -0500

    Rename to EtcdAppConfiguration

--- src/main/java/com/michael/container/config/EtcdAppConfiguration.java
+++ src/main/java/com/michael/container/config/EtcdAppConfiguration.java
@@ -1,14 +1,15 @@
 package com.michael.container.config;
 
+import com.michael.container.distributed.election.config.EtcdConfiguration;
 import io.etcd.jetcd.Client;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class EtcdConfiguration {
+public class EtcdAppConfiguration {
   @Bean
   public Client etcdClient(
-      com.michael.container.distributed.election.config.EtcdConfiguration etcdConfiguration) {
+      EtcdConfiguration etcdConfiguration) {
     return Client.builder().endpoints(etcdConfiguration.getEtcdEndpoints()).build();
   }
 }
