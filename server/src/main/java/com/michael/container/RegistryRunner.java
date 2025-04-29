dfcbee93c4ebebc9745ad620f3c2ceef47250717
Michael Rasch <raschmichael10@gmail.com>
2025-04-28 20:54:59 -0400

    Separate Main Class to New Module

--- server/src/main/java/com/michael/container/RegistryRunner.java
+++
@@ -1,9 +0,0 @@
-package com.michael.container;
-
-// @SpringBootApplication
-// public class RegistryRunner {
-//
-//  public static void main(String[] args) {
-//    SpringApplication.run(RegistryRunner.class);
-//  }
-// }
