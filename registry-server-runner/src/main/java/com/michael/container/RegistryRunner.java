af0ceee4f12016bcb7de3d8e7d60aaaa1e929a39
Michael Rasch <raschmichael10@gmail.com>
2025-04-28 21:01:56 -0400

    Fix .github and docker files

--- registry-server-runner/src/main/java/com/michael/container/RegistryRunner.java
+++ registry-server-runner/src/main/java/com/michael/container/RegistryRunner.java
@@ -1,9 +1,12 @@
 package com.michael.container;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class RegistryRunner {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RegistryRunner.class);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(RegistryRunner.class);
+  }
 }
