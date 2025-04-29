8940a2f98d31d4621d8785e56a0d5eb56cbd07da
Michael Rasch <raschmichael10@gmail.com>
2025-04-28 21:07:35 -0400

    Fixed scan issue with redis

--- server/src/main/java/com/michael/container/auto/ScanPackages.java
+++ server/src/main/java/com/michael/container/auto/ScanPackages.java
@@ -3,10 +3,8 @@ package com.michael.container.auto;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @ComponentScan("com.michael.container")
 @Configuration
 @ConditionalOnProperty(value = "service.registry.enabled", havingValue = "true")
-@EnableRedisRepositories(basePackages = "com.michael.container")
 public class ScanPackages {}
