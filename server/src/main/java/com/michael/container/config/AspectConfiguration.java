2ec00dfd017d0f38633c51c075422c405a2e2361
Michael Rasch <raschmichael10@gmail.com>
2025-05-09 19:14:19 -0400

    app properties added

--- server/src/main/java/com/michael/container/config/AspectConfiguration.java
+++ server/src/main/java/com/michael/container/config/AspectConfiguration.java
@@ -4,15 +4,15 @@ import com.michael.spring.utils.logger.handlers.ExecutionTimeAspectHandler;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
-@EnableAspectJAutoProxy
-@ConditionalOnProperty(value = "service.registry.aspectJ.enabled", havingValue = "true")
+@ConditionalOnProperty(value = "service.registry.logging.aspectJ.enabled", havingValue = "true")
 public class AspectConfiguration {
 
   @Bean
-  @ConditionalOnProperty(value = "serivce.registry.aspectJ.timing.enabled", havingValue = "true")
+  @ConditionalOnProperty(
+      value = "service.registry.logging.aspectJ.timing.enabled",
+      havingValue = "true")
   public ExecutionTimeAspectHandler executionTimeAspectHandler() {
     return new ExecutionTimeAspectHandler();
   }
